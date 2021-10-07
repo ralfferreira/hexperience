0561d5f036d91d12601701b66dd63424005c891c
Ralf Ferreira <ralfferreira995@gmail.com>
2021-10-07 16:33:56 -0300

    Add CreateExperience Estructure

--- src/pages/CreateExperience/index.js
+++ src/pages/CreateExperience/index.js
@@ -6,6 +6,66 @@ const CreateExperience = () => {
   return (
     <Container>
       <HeaderWithoutSearch>Nova Experiência</HeaderWithoutSearch>
+      <CreateExperience>
+        <Title>Título da Experiência</Title>
+        <ExperienceTitle />
+
+        <Title>Imagens</Title>
+        <ExperienceImage />
+
+        <Title>Descrição</Title>
+        <ExperienceDescription />
+
+        <Title>Detalhes</Title>
+        <ExperienceDetails>
+          <ExperienceAddress>
+            <AddressIcon />
+            <AddressInput />
+          </ExperienceAddress>
+          <ExperienceReferencePoint>
+            <ReferencePointIcon />
+            <ReferencePointInput />
+          </ExperienceReferencePoint>
+          <ExperienceDuration>
+            <DurationIcon />
+            <DurationInput />
+          </ExperienceDuration>
+          <ExperienceAmountPeople>
+            <AmountPeopleIcon />
+            <AmountPeopleInput />
+          </ExperienceAmountPeople>
+          <ExperiencePrice>
+            <PriceIcon />
+            <PriceInput />
+          </ExperiencePrice>
+        </ExperienceDetails>
+
+        <Title>Classificação Indicativa</Title>
+        <ParentalRating>
+          <ParentalRatingOption>
+            <FreeIcon />
+          </ParentalRatingOption>
+
+          <ParentalRatingOption>
+            <TenYearsIcon />
+          </ParentalRatingOption>
+
+          <ParentalRatingOption>
+            <TwelveYearsIcon />
+          </ParentalRatingOption>
+
+          <ParentalRatingOption>
+            <FourteenYearsIcon />
+          </ParentalRatingOption>
+
+          <ParentalRatingOption>
+            <EighteenYearsIcon />
+          </ParentalRatingOption>
+        </ParentalRating>
+
+        <Title>O Que Levar? (Opcional)</Title>
+        <ExperienceDescription />
+      </CreateExperience>
     </Container>
   );
 };
