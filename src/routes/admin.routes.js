cb0bb1360077d9324e76e20966bd63041bff9e75
Ralf Ferreira <ralfferreira995@gmail.com>
2021-11-03 13:30:17 -0300

    Add Admin Routes

---
+++ src/routes/admin.routes.js
@@ -0,0 +1,32 @@
+// import React from "react";
+// import { createStackNavigator } from "@react-navigation/stack";
+// import Admin from "../pages/Admin";
+// import HostRequests from "../pages/HostRequests";
+// import ComplaintUsers from "../pages/ComplaintUsers";
+// import BlockedUsers from "../pages/BlockedUsers";
+// import BlockedExperiences from "../pages/BlockedExperiences";
+// import ReportedExperiences from "../pages/ReportedExperiences";
+// import ReportedBugs from "../pages/ReportedBugs";
+
+// const AdminRoute = createStackNavigator();
+
+// const AdminDetails = () => (
+//   <AdminRoute.Navigator
+//     screenOptions={{
+//       headerShown: false,
+//   }}>
+//     <AdminRoute.Screen name="Admin" component={Admin} />
+
+//     <AdminRoute.Screen name="HostRequests" component={HostRequests} />
+//     <AdminRoute.Screen name="ComplaintUsers" component={ComplaintUsers} />
+//     <AdminRoute.Screen name="BlockedUsers" component={BlockedUsers} />
+
+//     <AdminRoute.Screen name="BlockedExperiences" component={BlockedExperiences} />
+//     <AdminRoute.Screen name="ReportedExperiences" component={ReportedExperiences} />
+
+//     <AdminRoute.Screen name="ReportedBugs" component={ReportedBugs} />
+    
+//   </AdminRoute.Navigator>
+// );
+
+// export default AdminDetails;
