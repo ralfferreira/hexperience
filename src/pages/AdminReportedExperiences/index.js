9eb369b59a3991ff1654e8bcf58593f8482ddc7c
Ralf Ferreira <ralfferreira995@gmail.com>
2021-11-23 17:44:44 -0300

    Add Admin Reported Experiences Page

--- src/pages/AdminReportedExperiences/index.js
+++ src/pages/AdminReportedExperiences/index.js
@@ -1,10 +1,70 @@
 import React from 'react'
-import { Container } from './styles';
-
+import { Container, Content, ReportedExperiencesList, ReportedExperiencesRow, ReportedExperiencesItem, ReportedExperiencesHeader, ReportedExperiencesImage, ReportedExperiencesName, ReportedExperiencesAlert, ReportedExperiencesDate  } from './styles';
+import { SearchBar } from 'react-native-elements';
+import Luffy from '../../assets/img/luffy.jpg';
 const AdminReportedExperiences = () => {
   return (
     <Container>
-      
+      <SearchBar
+        placeholder="Digite aqui..."
+        containerStyle={{
+          backgroundColor: '#fff',
+          borderWidth:  1,
+          borderColor: '#fff',
+          borderRightColor: '#fff',
+          borderBottomColor: '#ddd',
+          borderTopColor: '#fff',
+        }}
+        inputContainerStyle={{
+          backgroundColor: '#fff'}}
+        searchIcon={{ 
+          size: 24,
+          color: '#000' }}
+        platform="default"
+      />
+      <Content>
+        <ReportedExperiencesList>
+          <ReportedExperiencesRow>
+            <ReportedExperiencesItem>
+              <ReportedExperiencesHeader>
+                <ReportedExperiencesImage source={Luffy} />
+              </ReportedExperiencesHeader>
+              <ReportedExperiencesName>Passeio com Monkey D. Luffy</ReportedExperiencesName>
+              <ReportedExperiencesAlert>Falta de Segurança</ReportedExperiencesAlert>
+              <ReportedExperiencesDate>23/11/2021</ReportedExperiencesDate>
+            </ReportedExperiencesItem>
+
+            <ReportedExperiencesItem>
+              <ReportedExperiencesHeader>
+                <ReportedExperiencesImage source={Luffy} />
+              </ReportedExperiencesHeader>
+              <ReportedExperiencesName>Passeio com Monkey D. Luffy</ReportedExperiencesName>
+              <ReportedExperiencesAlert>Falta de Segurança</ReportedExperiencesAlert>
+              <ReportedExperiencesDate>23/11/2021</ReportedExperiencesDate>
+            </ReportedExperiencesItem>
+          </ReportedExperiencesRow>
+
+          <ReportedExperiencesRow>
+            <ReportedExperiencesItem>
+              <ReportedExperiencesHeader>
+                <ReportedExperiencesImage source={Luffy} />
+              </ReportedExperiencesHeader>
+              <ReportedExperiencesName>Passeio com Monkey D. Luffy</ReportedExperiencesName>
+              <ReportedExperiencesAlert>Falta de Segurança</ReportedExperiencesAlert>
+              <ReportedExperiencesDate>23/11/2021</ReportedExperiencesDate>
+            </ReportedExperiencesItem>
+
+            <ReportedExperiencesItem>
+              <ReportedExperiencesHeader>
+                <ReportedExperiencesImage source={Luffy} />
+              </ReportedExperiencesHeader>
+              <ReportedExperiencesName>Passeio com Monkey D. Luffy</ReportedExperiencesName>
+              <ReportedExperiencesAlert>Falta de Segurança</ReportedExperiencesAlert>
+              <ReportedExperiencesDate>23/11/2021</ReportedExperiencesDate>
+            </ReportedExperiencesItem>
+          </ReportedExperiencesRow>
+        </ReportedExperiencesList>
+      </Content>
     </Container>
   );
 };
