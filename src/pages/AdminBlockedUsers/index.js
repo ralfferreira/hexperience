6a0b9be1c90592f34f4befff95ead3d1156e35c5
Ralf Ferreira <ralfferreira995@gmail.com>
2021-11-23 17:20:18 -0300

    Add Admin Blocked Users Page

--- src/pages/AdminBlockedUsers/index.js
+++ src/pages/AdminBlockedUsers/index.js
@@ -1,9 +1,74 @@
 import React from 'react'
-import { Container } from './styles';
-
+import { Container, Content, RequestList, RequestItem, RequestItemProfile, RequestItemHeader, RequestItemName, RequestItemNickname, RequestItemID, RequestItemDate, RequestListRow } from './styles';
+import { SearchBar } from 'react-native-elements';
+import Luffy from '../../assets/img/luffy.jpg';
 const AdminBlockedUsers = () => {
   return (
     <Container>
+      <SearchBar
+        placeholder="Digite aqui..."
+        containerStyle={{
+          backgroundColor: '#fff',
+          borderWidth:  1,
+          borderColor: '#fff',
+          borderRightColor: '#fff',
+          borderBottomColor: '#ddd',
+          borderTopColor: '#fff',
+        }}
+        inputContainerStyle={{
+          backgroundColor: '#fff'}}
+        searchIcon={{ 
+          size: 24,
+          color: '#000' }}
+        platform="default"
+      />
+      <Content>
+        <RequestList>
+          <RequestListRow>
+            <RequestItem>
+              <RequestItemHeader>
+                <RequestItemProfile source={Luffy} />
+              </RequestItemHeader>
+              <RequestItemName>Monkey D. Luffy</RequestItemName>
+              <RequestItemNickname>Luffy</RequestItemNickname>
+              <RequestItemID>113.857.956-11</RequestItemID>
+              <RequestItemDate>23/11/2021</RequestItemDate>
+            </RequestItem>
+
+            <RequestItem>
+              <RequestItemHeader>
+                <RequestItemProfile source={Luffy} />
+              </RequestItemHeader>
+              <RequestItemName>Monkey D. Luffy</RequestItemName>
+              <RequestItemNickname>Luffy</RequestItemNickname>
+              <RequestItemID>113.857.956-11</RequestItemID>
+              <RequestItemDate>23/11/2021</RequestItemDate>
+            </RequestItem>
+          </RequestListRow>
+
+          <RequestListRow>
+            <RequestItem>
+              <RequestItemHeader>
+                <RequestItemProfile source={Luffy} />
+              </RequestItemHeader>
+              <RequestItemName>Monkey D. Luffy</RequestItemName>
+              <RequestItemNickname>Luffy</RequestItemNickname>
+              <RequestItemID>113.857.956-11</RequestItemID>
+              <RequestItemDate>23/11/2021</RequestItemDate>
+            </RequestItem>
+
+            <RequestItem>
+              <RequestItemHeader>
+                <RequestItemProfile source={Luffy} />
+              </RequestItemHeader>
+              <RequestItemName>Monkey D. Luffy</RequestItemName>
+              <RequestItemNickname>Luffy</RequestItemNickname>
+              <RequestItemID>113.857.956-11</RequestItemID>
+              <RequestItemDate>23/11/2021</RequestItemDate>
+            </RequestItem>
+          </RequestListRow>
+        </RequestList>
+      </Content>
     </Container>
   );
 };
