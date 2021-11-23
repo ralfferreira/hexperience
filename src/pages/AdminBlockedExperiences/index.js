c96ad5e3322c24f01423d021e9d966f84eed5e6c
Ralf Ferreira <ralfferreira995@gmail.com>
2021-11-23 17:39:54 -0300

    Add Admin Blocked Experiences Page

--- src/pages/AdminBlockedExperiences/index.js
+++ src/pages/AdminBlockedExperiences/index.js
@@ -1,9 +1,70 @@
 import React from 'react'
-import { Container } from './styles';
-
+import { Container, Content, BlockedExperiencesList, BlockedExperiencesRow, BlockedExperiencesItem, BlockedExperiencesHeader, BlockedExperiencesImage, BlockedExperiencesName, BlockedExperiencesAlert, BlockedExperiencesDate  } from './styles';
+import { SearchBar } from 'react-native-elements';
+import Luffy from '../../assets/img/luffy.jpg';
 const AdminBlockedExperiences = () => {
   return (
     <Container>
+      <SearchBar
+        placeholder="Digite aqui..."
+        containerStyle={{
+          backgroundColor: '#fff',
+          borderWidth:  1,
+          borderColor: '#fff',
+          borderRightColor: '#fff',
+          borderBottomColor: '#ddd',
+          borderTopColor: '#fff',
+        }}
+        inputContainerStyle={{
+          backgroundColor: '#fff'}}
+        searchIcon={{ 
+          size: 24,
+          color: '#000' }}
+        platform="default"
+      />
+      <Content>
+        <BlockedExperiencesList>
+          <BlockedExperiencesRow>
+            <BlockedExperiencesItem>
+              <BlockedExperiencesHeader>
+                <BlockedExperiencesImage source={Luffy} />
+              </BlockedExperiencesHeader>
+              <BlockedExperiencesName>Passeio com Monkey D. Luffy</BlockedExperiencesName>
+              <BlockedExperiencesAlert>Falta de Segurança</BlockedExperiencesAlert>
+              <BlockedExperiencesDate>23/11/2021</BlockedExperiencesDate>
+            </BlockedExperiencesItem>
+
+            <BlockedExperiencesItem>
+              <BlockedExperiencesHeader>
+                <BlockedExperiencesImage source={Luffy} />
+              </BlockedExperiencesHeader>
+              <BlockedExperiencesName>Passeio com Monkey D. Luffy</BlockedExperiencesName>
+              <BlockedExperiencesAlert>Falta de Segurança</BlockedExperiencesAlert>
+              <BlockedExperiencesDate>23/11/2021</BlockedExperiencesDate>
+            </BlockedExperiencesItem>
+          </BlockedExperiencesRow>
+
+          <BlockedExperiencesRow>
+            <BlockedExperiencesItem>
+              <BlockedExperiencesHeader>
+                <BlockedExperiencesImage source={Luffy} />
+              </BlockedExperiencesHeader>
+              <BlockedExperiencesName>Passeio com Monkey D. Luffy</BlockedExperiencesName>
+              <BlockedExperiencesAlert>Falta de Segurança</BlockedExperiencesAlert>
+              <BlockedExperiencesDate>23/11/2021</BlockedExperiencesDate>
+            </BlockedExperiencesItem>
+
+            <BlockedExperiencesItem>
+              <BlockedExperiencesHeader>
+                <BlockedExperiencesImage source={Luffy} />
+              </BlockedExperiencesHeader>
+              <BlockedExperiencesName>Passeio com Monkey D. Luffy</BlockedExperiencesName>
+              <BlockedExperiencesAlert>Falta de Segurança</BlockedExperiencesAlert>
+              <BlockedExperiencesDate>23/11/2021</BlockedExperiencesDate>
+            </BlockedExperiencesItem>
+          </BlockedExperiencesRow>
+        </BlockedExperiencesList>
+      </Content>
     </Container>
   );
 };
--- src/pages/AdminBlockedExperiences/styles.js
+++ src/pages/AdminBlockedExperiences/styles.js
@@ -1,5 +1,55 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-
+    background: #fff;
+    height: 100%;
 `;
+export const Content = styled.ScrollView`
+    height: 100%;
+`
+export const BlockedExperiencesList = styled.View`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding: 15px;
+    margin-bottom: 100px;
+`
+export const BlockedExperiencesItem = styled.View`
+    width: 180px;
+    height: 275px;
+    background: #FCFCFC;
+    box-shadow: 0px 1px 20px #B5B5B5;
+    elevation: 5;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-bottom: 20px;
+`
+export const BlockedExperiencesRow = styled.View`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+export const BlockedExperiencesHeader = styled.View`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+export const BlockedExperiencesImage = styled.Image`
+    width: 100%;
+    height: 120px;
+`
+export const BlockedExperiencesName = styled.Text`
+    font-size: 16px;
+    padding: 5px;
+`
+export const BlockedExperiencesAlert = styled.Text`
+    font-size: 13px;
+    padding: 5px;
+`
+export const BlockedExperiencesDate = styled.Text`
+    font-size: 10px;
+    padding: 5px;
+    text-align: right;
+    color: #161616;
+`
